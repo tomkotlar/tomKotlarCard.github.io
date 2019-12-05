4a49374abbb35071216b46274b179b866eef53c9
tomkotlar <t.kotlar@yahoo.com>
2019-12-05 21:19:53 +0000

    adding dark theme toggler

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function showPage() {
   document.querySelector(".loader").style.display = "none";
   document.querySelector("#myDiv").style.display = "block";
 }
+
+const check = document.getElementById('checkbox');
+
+check.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
